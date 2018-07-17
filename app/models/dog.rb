dc85d0d3008390967530e976fc2fee7604bd7420
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-17 17:58:08 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,18 @@
 class Dog < Sinatra::Base 
 attr_accessor :name, :breed, :age 
-@how_many_dogs= 0 
+@@how_many_dogs= 0 
+@@all_dogs=[]
 
 def initialize(name, breed, age)
   @name = name 
   @breed = breed 
   @age = age 
-  @how_many_dogs+=1 
+  @@how_many_dogs+=1 
+  @@all_dogs<<@name 
 end 
   
 def all 
-  all = []  
+  puts all_dogs  
 end 
 
 end 
