742c4438ae9f247056b61da0db8243f1e891aaff
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-17 17:58:24 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,18 +1,18 @@
 class Dog < Sinatra::Base 
-attr_accessor :name, :breed, :age 
-@@how_many_dogs= 0 
-@@all_dogs=[]
-
-def initialize(name, breed, age)
-  @name = name 
-  @breed = breed 
-  @age = age 
-  @@how_many_dogs+=1 
-  @@all_dogs<<@name 
-end 
+  attr_accessor :name, :breed, :age 
+  @@how_many_dogs= 0 
+  @@all_dogs=[]
   
-def all 
-  puts all_dogs  
-end 
+  def initialize(name, breed, age)
+    @name = name 
+    @breed = breed 
+    @age = age 
+    @@how_many_dogs+=1 
+    @@all_dogs<<@name 
+  end 
+    
+  def all 
+    puts all_dogs  
+  end 
 
 end 
